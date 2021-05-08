5a1b0954ec06535d5f0265f8b62899a2d48cda01
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-08 10:38:49 +0530

    div function

--- calculation.sh
+++ calculation.sh
@@ -1,6 +1,15 @@
 #! /bin/bash -x
 echo "welcome to calculation"
 
+function div() {
+
+	local a=$1
+	local b=$2
+	local c=$(echo $a $b | awk '{print $1/$b}' )
+	echo $c
+
+}
+
 function add() {
 	local a=$1
 	local b=$2
@@ -23,3 +32,6 @@ echo $result
 
 resultSub="$(sub $a $b)"
 echo $resultSub
+
+resutlDiv="$(div $a $b)"
+echo $resultDiv
